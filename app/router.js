60164a7288065e3d1b2ff91daa854a196811e665
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2017-07-25 16:08:04 +0545

    Add bootstrap

--- app/router.js
+++ app/router.js
@@ -1,13 +1,14 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from 'ember'
+import config from './config/environment'
 
 const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
-});
+})
 
-Router.map(function() {
-  this.route('leads');
-});
+Router.map(function () {
+  this.route('leads')
+  this.mount('super-blog', { path: '/engine' })
+})
 
-export default Router;
+export default Router
--- app/routes/leads.js
+++ app/routes/leads.js
@@ -1,26 +1,26 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default Ember.Route.extend({
 
-  model(){
-    return this.store.findAll('lead');
+  model () {
+    return this.store.findAll('lead')
   },
-  setupController(controller, model){
-    this._super(...arguments);
-    controller.set('filteredList', model);
+  setupController (controller, model) {
+    this._super(...arguments)
+    controller.set('filteredList', model)
   },
-  actions:{
-    search(searchName){
-      if(Ember.isEmpty(searchName)){
-          this.controller.set('filteredList', this.controller.get('model'));
-          return;
+  actions: {
+    search (searchName) {
+      if (Ember.isEmpty(searchName)) {
+        this.controller.set('filteredList', this.controller.get('model'))
+        return
       }
-      let leadFound =  this.controller.get('model').filter((lead) => {
-        return lead.get("firstName").toLowerCase() == searchName.toLowerCase();
-      });
-      if(leadFound){
-        this.controller.set('filteredList', leadFound);
+      let leadFound = this.controller.get('model').filter((lead) => {
+        return lead.get('firstName').toLowerCase() === searchName.toLowerCase()
+      })
+      if (leadFound) {
+        this.controller.set('filteredList', leadFound)
       }
     }
   }
-});
+})
