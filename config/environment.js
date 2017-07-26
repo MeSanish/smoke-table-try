c429a1706406bd8d932b0b95577e3290051d86d1
Sanish Pradhananga <sanish.pradhananga@fusemachines.com>
2017-07-26 09:58:44 +0545

    p#9

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.rootURL = '/smoke-table-try';
-    ENV.locationType = 'hash';
+    ENV.locationType = 'none';
     ENV['ember-cli-mirage'] = {
       enabled: true
     }
